d65d1b1e815eae92c39f26862d0c72d564da8767
Akshay rathore <akshayrathore9010@gmail.com>
2025-05-17 15:12:21 +0530

    Add frontend assets and serve from backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,11 +2,9 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-
 import path from "path";
 
 import { connectDB } from "./lib/db.js";
-
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { app, server } from "./lib/socket.js";
@@ -28,11 +26,12 @@ app.use(
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
+// CHANGED: Serve frontend from dist inside backend folder
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "dist", "index.html"));
   });
 }
 
